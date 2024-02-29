e3ced1c4a6dac55817b1ff5507f7ace08e832559
gabeyy <gabriel.gruenberg@icloud.com>
2024-02-29 20:29:53 +0100

    Add total expenses to monthly chart

--- Components/UI/MonthlyChart.js
+++ Components/UI/MonthlyChart.js
@@ -4,18 +4,36 @@ import { PieChart } from "react-native-gifted-charts";
 import { styles } from "../../src/style";
 
 const chartData = [
-  { text: "📅", value: Math.random() * 100 },
-  { text: "🛒", value: Math.random() * 100 },
-  { text: "🚗", value: Math.random() * 100 },
-  { text: "🧘", value: Math.random() * 100 },
-  { text: "💰", value: Math.random() * 100 },
+  { text: "📅", value: Math.random() * 100, color: "#bf616a" },
+  { text: "🛒", value: Math.random() * 100, color: "#d08770" },
+  { text: "🚗", value: Math.random() * 100, color: "#ebcb8b" },
+  { text: "🧘", value: Math.random() * 100, color: "#a3be8c" },
+  { text: "💰", value: Math.random() * 100, color: "#b48ead" },
 ];
+
+const totalExpenses = () => {
+  return (
+    <Text>
+      {new Intl.NumberFormat("de-DE", {
+        style: "currency",
+        currency: "EUR",
+      }).format(Math.random() * 1000)}
+    </Text>
+  );
+};
 
 export default function MonthlyChart() {
   return (
     <View style={styles.MonthlyChart}>
       <Text style={styles.headingSmall}>Expenses this month</Text>
-      <PieChart data={chartData} showText={true} radius={150} />
+      <PieChart
+        data={chartData}
+        centerLabelComponent={totalExpenses}
+        donut
+        showText
+        focusOnPress
+        radius={150}
+      />
     </View>
   );
 }
--- src/style.js
+++ src/style.js
@@ -1,11 +1,17 @@
 import { StyleSheet } from "react-native";
 
+const background = "#ffffff";
+const backgroundAccent = "#ECEFF4";
+const fontColor = "#2e3440";
+const buttonColor = "#88c0d0";
+
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: background,
     alignItems: "center",
     justifyContent: "center",
+    color: fontColor,
   },
 
   balanceView: {
@@ -24,5 +30,13 @@ export const styles = StyleSheet.create({
   accountBalance: {
     fontSize: 32,
     textAlign: "center",
+  },
+
+  button: {
+    backgroundColor: buttonColor,
+    color: fontColor,
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 8,
   },
 });
